f28ff65bbbc0d84f0bf73c5fed1a24752ae26792
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-06 18:36:54 -0600

    add routes

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { db } = require('db');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+// middleware for  project
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes for project
+app.use("api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
